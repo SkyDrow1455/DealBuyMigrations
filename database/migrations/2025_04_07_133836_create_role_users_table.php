97312bfa9234296df1a6542d5497f89a01c5ac86
CamiloSamboni <camiloandressamboni55@gmail.com>
2025-04-07 08:52:15 -0500

    roleUSers

--- database/migrations/2025_04_07_133836_create_role_users_table.php
+++ database/migrations/2025_04_07_133836_create_role_users_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('role_users', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
